f07237ca5f42d7b7b337a35c8ce6728227a982c8
nora-nz <91670740+nora-nz@users.noreply.github.com>
2022-07-27 11:32:18 +0100

    Add files via upload

---
+++ docs/removed_logic.js
@@ -0,0 +1,14 @@
+const elem = document.getElementById('3d-graph');
+
+const Graph = ForceGraph3D()(elem)
+    .graphData(gData)
+    .nodeVisibility(node => node.group !== "z")
+    .nodeRelSize(5)
+    .nodeColor(node => node.opacity ? "rgba(255, 255, 255, 0.2)" : node.group === "c" ? "red" : node.group === "g" ? "pink" : node.group === "l" ? "orange" : node.group === "m" ? "yellow" : node.group === "n" ? "green" : node.group === "p" ? "teal" : node.group === "r" ? "blue" : "purple")
+    .linkDirectionalParticles(link => link.particle ? 1 : 0)
+    .linkDirectionalParticleSpeed(0.002)
+    .linkDirectionalParticleWidth(2)
+    .warmupTicks(100)
+    .cooldownTicks(0)
+    .linkColor("colour")
+    .linkDirectionalParticleColor("colour");
